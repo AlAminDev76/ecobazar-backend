da2599d1615a30ea6b2582806ea8221fe9d530c9
Alamin Khan <alamin@example.com>
2026-05-09 18:59:31 +0600

    configuration

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+require('dotenv').config()
 const express = require('express')
-
 const app = express()
+const cors = require('cors')
+const bdConfig = require("./config/dbConfig")
+
+//middleware
+app.use(express.json())
+app.use (cors())
+
+//database Config
+bdConfig()
 
 app.get('/',(req,res)=>{
    res.send("hello developer")
 })
+let port = process.env.port || 5000
 app.listen(5000,()=>{
-    console.log("server running on port 5000")
+    console.log(`server running on port ${port}`)
 })
